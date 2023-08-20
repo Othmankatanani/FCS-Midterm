66f7eb2c12b7a85e32bf5fa6ef3d061f3e6cf1b8
othman katanani <othman.katanani.91@gmail.com>
2023-08-20 04:12:49 +0300

    add two classes and file.txt

--- MidtermExamProject.py
+++ MidtermExamProject.py
@@ -5,3 +5,19 @@ class Employee:
         self.date_joined = date_joined
         self.gender = gender
         self.salary = salary
+
+class EmployeeSystem:
+    def __init__(self):
+        self.employees = {}
+        self.load_employees_from_file("employee_data.txt")
+        self.auto_increment_id = len(self.employees) + 1 
+
+    def load_employees_from_file(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    emp_id, username, date_joined, gender, salary = line.strip().split(', ')
+                    self.employees[emp_id] = Employee(emp_id, username, date_joined, gender, int(salary))
+        except FileNotFoundError:
+            print("File not found.")
