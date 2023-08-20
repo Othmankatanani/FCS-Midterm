940833970a511cea960de1824d9e6512da1929a4
othman katanani <othman.katanani.91@gmail.com>
2023-08-20 03:53:03 +0300

    update file

---
+++ MidtermExamProject.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, emp_id, username, date_joined, gender, salary):
+        self.emp_id = emp_id
+        self.username = username
+        self.date_joined = date_joined
+        self.gender = gender
+        self.salary = salary
